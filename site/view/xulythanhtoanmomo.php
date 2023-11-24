d19e14fa9c4bdc304c5beeaec2ccb57144e7d12d
TranTrung <trungtqps31229@fpt.edu.vn>
2023-11-24 15:04:58 +0700

    sửa thanh toán momo

--- site/view/xulythanhtoanmomo.php
+++ site/view/xulythanhtoanmomo.php
@@ -1,7 +1,13 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 
-
+session_start();
+$sum = 0; // Khởi tạo $sum ở đây
+foreach ($_SESSION['cart'] as $item) {
+    $productTotal = $item['GiaKhuyenMai'] * $item['sl'];
+    $sum += $productTotal; // Cộng dồn tổng tiền sản phẩm vào $sum
+    $sum += 30000;
+}
 function execPostRequest($url, $data)
 {
     $ch = curl_init($url);
@@ -30,7 +36,7 @@ $accessKey = 'klm05TvNBzhg7h7j';
 $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
 
 $orderInfo = "Thanh toán qua mã QR MoMo";
-$amount = "10000";
+$amount = $sum;
 $orderId = time() ."";
 $redirectUrl = "http://localhost/DuanWebsite/site/index.php?mod=cart&act=checkout";
 $ipnUrl = "http://localhost/DuanWebsite/site/index.php?mod=cart&act=checkout";
